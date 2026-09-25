7d75ccc8f7cef89a0ff41b34cba934d04cf12f99
GabrielJanuario0102 <gabriel.januario@academico.ifpb.edu.br>
2026-09-25 17:03:26 -0300

    feat: uso de gps na tela HOME

--- app/tabs/Home.jsx
+++ app/tabs/Home.jsx
@@ -1,12 +1,46 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useEffect, useState } from "react";
+
+import { Text, View } from "react-native";
+
+import * as Location from "expo-location";
 
 const Home = () => {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState("");
+  const [text, setText] = useState("Esperando permissão...");
+
+  useEffect(() => {
+    const buscarLocalizacao = async () => {
+      const { status } =
+        await Location.requestForegroundPermissionsAsync();
+
+      if (status !== "granted") {
+        setErrorMsg("Permissão de localização negada...");
+        return;
+      }
+
+      const location =
+        await Location.getCurrentPositionAsync({});
+
+      setLocation(location);
+    };
+
+    buscarLocalizacao();
+  }, []);
+
+  useEffect(() => {
+    if (errorMsg) {
+      setText(errorMsg);
+    } else if (location) {
+      setText(JSON.stringify(location));
+    }
+  }, [location, errorMsg]);
+
   return (
     <View>
-      <Text>Home</Text>
+      <Text>{text}</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
